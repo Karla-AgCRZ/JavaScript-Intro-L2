119a16c4af71903dc93ceaf8c7a3194bec3c2e9d
Karla CRZ <lar.crz.kl@gmail.com>
2025-03-20 20:16:16 -0600

    :sparkles: Mi primer código

---
+++ 01.Estruturas_de_control/main.js
@@ -0,0 +1,9 @@
+/// 1. Declarar una variable
+let age = 20
+
+// Implemento una condicional/estructura de control/ If ... else
+if (age >= 18) {
+    console.log("ERES MAYOR DE EDAD");
+}else {
+    console.log("ERES MENOR DE EDAD");
+}
